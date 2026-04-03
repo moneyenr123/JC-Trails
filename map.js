8d01fb74381b7eb0fc71a4aa9ab37dd289d8d02f
JC Unblocked <158098394+moneyenr123@users.noreply.github.com>
2026-04-03 08:25:34 -0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,357 @@
+{
+  "variant": "light",
+  "backgroundColor": "#F5F5F5",
+  "styles": [
+    {
+      "id": "natural.land",
+      "geometry": {
+        "fillColor": {
+          "z00": "#EAEFF2"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.crops",
+      "geometry": {
+        "fillColor": {
+          "z00": "#E2EFE0"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.dryCrops",
+      "geometry": {
+        "fillColor": {
+          "z00": "#E2EFE0"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.forest",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D9E8D7"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.ice",
+      "geometry": {
+        "fillColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.sand",
+      "geometry": {
+        "fillColor": {
+          "z00": "#FAF9F6"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.shrub",
+      "geometry": {
+        "fillColor": {
+          "z00": "#E2EFE0"
+        }
+      }
+    },
+    {
+      "id": "natural.land.landCover.tundra",
+      "geometry": {
+        "fillColor": {
+          "z00": "#E8E8E8"
+        }
+      }
+    },
+    {
+      "id": "natural.water",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C0E0F0"
+        }
+      },
+      "label": {
+        "textFillColor": "#005F80",
+        "textStrokeColor": "#E0F0F5"
+      }
+    },
+    {
+      "id": "infrastructure.urbanArea",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D3D6D9"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.noTraffic.trail",
+      "geometry": {
+        "fillColor": {
+          "z00": "#B8A398"
+        },
+        "strokeColor": {
+          "z00": "#9A8074"
+        },
+        "strokeWidth": {
+          "z00": 1.5,
+          "z12": 2.5
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#604030"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.noTraffic.trail.paved",
+      "geometry": {
+        "fillColor": {
+          "z00": "#B8A398"
+        },
+        "strokeColor": {
+          "z00": "#9A8074"
+        },
+        "strokeWidth": {
+          "z00": 1.5,
+          "z12": 2.5
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#604030"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.noTraffic.trail.unpaved",
+      "geometry": {
+        "fillColor": {
+          "z00": "#B8A398"
+        },
+        "strokeColor": {
+          "z00": "#9A8074"
+        },
+        "strokeWidth": {
+          "z00": 1.5,
+          "z12": 2.5
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#604030"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.road.highway",
+      "geometry": {
+        "fillColor": {
+          "z00": "#A0A8AE"
+        },
+        "strokeColor": {
+          "z00": "#80888E"
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#444444"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.road.arterial",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C0C8CD"
+        },
+        "strokeColor": {
+          "z00": "#A0A8AE"
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#555555"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "infrastructure.roadNetwork.road.local",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D0D8DC"
+        },
+        "strokeColor": {
+          "z00": "#B0B8BC"
+        }
+      },
+      "label": {
+        "textFillColor": {
+          "z00": "#666666"
+        },
+        "textStrokeColor": {
+          "z00": "#FFFFFF"
+        }
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.park",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.zoo",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.sportsField",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.golfCourse",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "pointOfInterest.other.cemetery",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D8E2D5"
+        }
+      },
+      "label": {
+        "textFillColor": "#546e7a",
+        "textStrokeColor": "#ffffff"
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.natureReserve",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "infrastructure.businessCorridor",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D3D6D9"
+        }
+      }
+    },
+    {
+      "id": "pointOfInterest.recreation.sportsComplex",
+      "geometry": {
+        "fillColor": {
+          "z00": "#C7E6C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#338A33",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "pointOfInterest.other.military",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D8DBDE"
+        }
+      },
+      "label": {
+        "textFillColor": "#546e7a",
+        "textStrokeColor": "#ffffff"
+      }
+    },
+    {
+      "id": "pointOfInterest.emergency.hospital",
+      "geometry": {
+        "fillColor": {
+          "z00": "#FFE0E0"
+        }
+      },
+      "label": {
+        "textFillColor": "#E05D5D",
+        "textStrokeColor": "#FFFFFF"
+      }
+    },
+    {
+      "id": "political.reservation",
+      "geometry": {
+        "fillColor": {
+          "z00": "#E0E0E0"
+        },
+        "strokeColor": {
+          "z00": "#C0C0C0"
+        }
+      },
+      "label": {
+        "textFillColor": "#757575"
+      }
+    },
+    {
+      "id": "pointOfInterest.transit.airport",
+      "geometry": {
+        "fillColor": {
+          "z00": "#D3D6D9"
+        }
+      },
+      "label": {
+        "textFillColor": "#506C7A"
+      }
+    }
+  ]
+}
